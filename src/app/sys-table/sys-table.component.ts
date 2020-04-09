3c33f1b89f28bbdc03ddc01712ffcf0bb6ba6110
indianazhao <indiana.zhao@gmail.com>
2020-04-10 03:23:37 +0800

    [03] Parameterize the component 'sys-table'.

--- src/app/sys-table/sys-table.component.ts
+++ src/app/sys-table/sys-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sys-table',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sys-table.component.scss']
 })
 export class SysTableComponent implements OnInit {
+
+  @Input() header: string;
+  @Input() items: string[];
 
   constructor() { }
 
