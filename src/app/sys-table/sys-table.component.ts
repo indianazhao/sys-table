7a4a41eddb06974af2f31af58ff0ab91dfbc5bca
indianazhao <indiana.zhao@gmail.com>
2020-04-10 04:06:47 +0800

    [05] Get grouping data.

--- src/app/sys-table/sys-table.component.ts
+++ src/app/sys-table/sys-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-sys-table',
@@ -10,9 +11,25 @@ export class SysTableComponent implements OnInit {
   @Input() header: string[];
   @Input() data: any[];
 
+  groups: any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.grouping(this.data);
+  }
+
+  grouping(data: any) {
+    this.groups = _.chain(data)
+                   .groupBy('gender')
+                   .toPairs()
+                   .map(value => {
+                     return {
+                       id: value[0],
+                       items: value[1]
+                     };
+                   })
+                   .value();
   }
 
 }
